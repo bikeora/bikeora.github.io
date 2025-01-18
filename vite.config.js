5329662fa3318cf5c3ffa1309725d8bd26c09f7a
iTools <fuhao789@hotmail.com>
2025-01-18 20:09:35 +0800

    init

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  build: {
+    // 指定打包输出目录
+    outDir: ".", // 你可以将 'dist' 替换为你想要的目录名称
+    // 确保清理目录时不删除根目录中的其他文件
+    emptyOutDir: false,
+  },
+});
